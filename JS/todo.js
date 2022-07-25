ad3fec6d8eb1ff250a35202d60ce9ddf4313593e
rktdnjs <rktdnjs@naver.com>
2022-07-25 23:02:51 +0900

    으악

--- JS/todo.js
+++ JS/todo.js
@@ -2,12 +2,28 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+const TODOS_KEY = "todos";
+
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo) {
     const list = document.createElement("li");
     const span = document.createElement("span");
-    list.appendChild(span);
     span.innerText = newTodo;
-    
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+
+    list.appendChild(span);
+    list.appendChild(button);
     toDoList.appendChild(list);
 }
 
@@ -16,7 +32,22 @@ function handleToDoSubmit(event) {
 
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+// function sayHello(item) {
+//     console.log("This is ", item);
+// }
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+console.log(savedToDos);
+if(savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos)
+    parsedToDos.forEach((item) => console.log("This is the turn of ", item));
+}
+
+// for each는 배열의 각 item에 대해서 function을 실행하게 해준다.
